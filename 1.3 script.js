e600e3b62e6d64b2bd324c3c595b9d2b3ea86dca
SAJNUS <sajnussaharearhojayfa@gmail.com>
2025-07-19 03:11:09 +0600

    Initial commit

---
+++ 1.3 script.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+    const menu = document.getElementById('dropdownMenu');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+}
