584f2c1036a313da4b2cdf23506043cfbacf98ad
SAJNUS <sajnussaharearhojayfa@gmail.com>
2025-08-02 22:13:45 +0600

    Delete Comments

--- Lab Report 4/db.php
+++ Lab Report 4/db.php
@@ -2,12 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "testdb"; // আগে XAMPP-এর phpMyAdmin থেকে এই নামে একটা DB বানিয়ে নাও
+$database = "testdb";
 
-// কানেকশন তৈরি করো
 $conn = new mysqli($servername, $username, $password, $database);
 
-// কানেকশন চেক করো
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
