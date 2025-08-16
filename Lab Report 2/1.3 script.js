64ebe453c521aa935d6a4f912def203220083013
SAJNUS <sajnussaharearhojayfa@gmail.com>
2025-08-16 21:24:11 +0600

    Some initial changes

--- Lab Report 2/1.3 script.js
+++
@@ -1,8 +0,0 @@
-function toggleDropdown() {
-    const menu = document.getElementById('dropdownMenu');
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
-}
--- Lab Report 4/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-?>
-<html>
-<head><title>PHP + MySQL Test</title></head>
-<body>
-<h1>PHP + MySQL Connection Successful</h1>
-</body>
-</html>
